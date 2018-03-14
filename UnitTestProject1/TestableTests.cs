144fd11e7b762023facd2cc3c941d75fe9c68627
Jini Byun <jinibyun@gmail.com>
2018-03-14 16:10:40 -0400

    c

---
+++ UnitTestProject1/TestableTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class TestableTests
+    {
+        [TestMethod()]
+        public void getMultiplyTest()
+        {
+            decimal a = 23.1M;
+            decimal b = 23.09M;
+
+            var obj = new Testable();
+            var result = obj.getMultiply(a, b);
+
+
+            Assert.IsNotNull(result, "result is null");
+        }
+    }
+}
